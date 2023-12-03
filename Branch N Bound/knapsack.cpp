259bed67d378f522f64fa5acfd5f2d484d00bfd1
lonnewolf120 <iftekharulislam360@gmail.com>
2023-12-03 23:50:16 +0600

    bb

--- Branch N Bound/knapsack.cpp
+++ Branch N Bound/knapsack.cpp
@@ -25,7 +25,7 @@ int main(){
     queue<node> q;
     node root={0,0,0,0};
     int max_profit=0;
-    //ub = vi + (W-wi)*(Vi-1/wi-1)
+    //ub = vi + (W-wi)*(Vi+1/wi+1)
     root.ub=root.profit+(W-root.wt)*(double)v[root.level].profit/v[root.level].wt;
 
     q.push(root);
@@ -36,26 +36,33 @@ int main(){
 
         if(k.level==n) continue;  //final level reached so traverse others
         
-        node child = {//node = wt, profit, level, ub
-            k.wt+v[k.level].wt,
-            k.profit+v[k.level].profit,
+ ////TAKE       
+        node take = {
+            k.wt+v[k.level].wt,     //weight = prev-weight + weight-next-elem
+            k.profit+v[k.level].profit,     //profit = prev-profit + profit-next-elem
+            k.level+1,  //level increase by 1
+            k.ub};  //ub
+
+        // ub = vi + (W-wi)*(Vi+1/wi+1), [here take's level is increased by one, so it is referring to the next elem]
+        take.ub = take.profit + (W-take.wt)*(double)v[take.level].profit/v[take.level].wt;
+
+        if(take.profit>max_profit and take.wt<=W) //make it max only if it's valid
+            max_profit=take.profit;
+
+        if(take.ub>max_profit)     //if greater element found push it for further iteration on that node
+            q.push(take);
+        
+ ////NOT-TAKE
+        node not_take = {
+            k.wt,
+            k.profit,
             k.level+1,
             k.ub};
 
-        child.ub = child.profit + (W-child.wt)*(double)v[child.level].profit/v[child.level].wt;
+        not_take.ub = not_take.profit+(W-not_take.wt)*(double)v[not_take.level].profit/v[not_take.level].wt;
 
-        if(child.wt<=W && child.profit>max_profit)
-            max_profit=child.profit;
-
-        if(child.ub>max_profit)
-            q.push(child);
-
-        child = {k.wt,k.profit,k.level+1,k.ub};
-
-        child.ub = child.profit+(W-child.wt)*(double)v[child.level].profit/v[child.level].wt;
-
-        if(child.ub > max_profit)
-            q.push(child);
+        if(not_take.ub > max_profit)
+            q.push(not_take);
     }
 
     cout<<max_profit<<endl;
